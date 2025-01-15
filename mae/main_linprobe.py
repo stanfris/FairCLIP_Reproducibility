a2905f8c450af59383ed130910cf67b9ffd3e881
Stan Fris <sfris@int5.local.snellius.surf.nl>
2025-01-15 09:32:35 +0100

    files for linearprobing

--- mae/main_linprobe.py
+++ mae/main_linprobe.py
@@ -135,7 +135,7 @@ def get_args_parser():
     return parser
 
 def main(args):
-    misc.init_distributed_mode(args)
+    # misc.init_distributed_mode(args)
 
     print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
     print("{}".format(args).replace(', ', ',\n'))
@@ -308,9 +308,9 @@ def main(args):
     print("accumulate grad iterations: %d" % args.accum_iter)
     print("effective batch size: %d" % eff_batch_size)
 
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-        model_without_ddp = model.module
+    #if args.distributed:
+    #    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+    #    model_without_ddp = model.module
 
     optimizer = LARS(model_without_ddp.head.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     print(optimizer)
@@ -332,8 +332,8 @@ def main(args):
     max_val_auc = -1
     best_epoch_test_stats = None
     for epoch in range(args.start_epoch, args.epochs):
-        if args.distributed:
-            data_loader_train.sampler.set_epoch(epoch)
+        #if args.distributed:
+        #    data_loader_train.sampler.set_epoch(epoch)
         train_stats = train_one_epoch(
             model, criterion, data_loader_train,
             optimizer, device, epoch, loss_scaler,
