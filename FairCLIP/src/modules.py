025508955f93e7cd93e61bef5a691326af711a42
Madelon <madelon.bernardy@live.nl>
2025-01-21 14:50:17 +0100

    fizxes bug in modules

--- FairCLIP/src/modules.py
+++ FairCLIP/src/modules.py
@@ -91,7 +91,7 @@ class fair_vl_med_dataset(torch.utils.data.Dataset):
         self.summarized_notes = {}
         # summarized_note_file is a csv file that contains the summarized notes associated with npz files
         # read the summarized notes from the csv file and construct a dictionary
-        if (self.subset == 'Training' or self.present_as_training) and self.text_source == 'note' and summarized_note_file != '':
+        if (self.subset == 'Training' or self.present_as_training) and self.text_source == 'note' and summarized_note_file is not None:
             df = pd.read_csv(os.path.join(dataset_dir, summarized_note_file))
             
             for _, row in df.iterrows():
