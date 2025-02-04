8d4240a3f71167629f70c83f240433b28e0d6740
Madelon <madelon.bernardy@live.nl>
2025-02-04 20:59:05 +0100

    hopefully works

--- FairCLIPlus/FairCLIPlus.py
+++ FairCLIPlus/FairCLIPlus.py
@@ -1,6 +1,9 @@
 from typing import Literal
 import torch
 import clip
+from torch.utils.data import DataLoader
+from src.modules import *
+from src import logger
 
 class FairCLIPlus(torch.nn.Module):
     """
@@ -10,9 +13,10 @@ class FairCLIPlus(torch.nn.Module):
     def __init__(self, attributes: dict[str, float],
                  model_architecture: Literal['RN50', 'ViT-B/16', 'ViT-L/14'],
                  device: Literal['cuda', 'cpu'],
-                 loss_img,
-                 loss_txt,
-                 distance_loss,
+                 loss_img: callable,
+                 loss_txt: callable,
+                 distance_loss: callable,
+                 fairness_weight: float,
                  pretrained_weights: str = ""):
         """
         Initializes the FairCLIPlus model.
@@ -25,12 +29,14 @@ class FairCLIPlus(torch.nn.Module):
             loss_img (callable): Loss function for image representations.
             loss_txt (callable): Loss function for text representations.
             distance_loss (callable): Loss function for measuring distance between correlations.
+            fairness_weight (float): Indicates how heavy the distance should count
             pretrained_weights (str, optional): Path to pretrained model weights. Defaults to "".
         """
         super().__init__()
 
         self.device = device
         self.attributes = attributes
+        self.fairness_weight = fairness_weight
 
         # Defining losses for the fairCLIP objective
         self.loss_img = loss_img
@@ -61,17 +67,15 @@ class FairCLIPlus(torch.nn.Module):
         logits_per_image, logits_per_text = self.model(images, texts)
         return logits_per_image, logits_per_text
 
-    def fairCLIPlus_loss(self, logits_per_image_batch, logits_per_text_batch, loss_img, loss_txt,
-                        logits_per_attr: dict[dict[str, (torch.Tensor, torch.Tensor)]]):
+    def fairCLIPlus_loss(self, logits_per_image_batch, logits_per_text_batch,
+                        logits_per_attr: dict[str, dict[int, (torch.Tensor, torch.Tensor)]]):
         """
         Computes the fairness-aware loss for the FairCLIPlus model.
 
         Args:
             logits_per_image_batch (torch.Tensor): Logits for images in the batch.
             logits_per_text_batch (torch.Tensor): Logits for texts in the batch.
-            loss_img (callable): Loss function for images.
-            loss_txt (callable): Loss function for texts.
-            logits_per_attr (dict[dict[str, (torch.Tensor, torch.Tensor)]]):
+            logits_per_attr (dict[str, dict[str, (torch.Tensor, torch.Tensor)]]):
                 Nested dictionary mapping attributes to image-text logits.
 
         Returns:
@@ -79,14 +83,18 @@ class FairCLIPlus(torch.nn.Module):
         """
         ground_truth = torch.arange(
                 len(logits_per_image_batch), dtype=torch.long, device=self.device)
-        total_loss_clip = (loss_img(logits_per_image_batch, ground_truth) +
-                        loss_txt(logits_per_text_batch, ground_truth)) / 2
+        total_loss_clip = (self.loss_img(logits_per_image_batch, ground_truth) +
+                        self.loss_txt(logits_per_text_batch, ground_truth)) / 2
 
         # Compute similarity scores
         similarity = logits_per_image_batch @ logits_per_text_batch.T
         correlations_with_batch = similarity.diag().float()
 
         # Compute fairness-related loss per attribute
+        # TODO: implement standardize
+        if self.fairness_weight == 0:
+            return total_loss_clip
+
         distance_loss = 0
         for attribute_name, attr_weight in self.attributes.items():
             if attr_weight == 0:
@@ -100,5 +108,178 @@ class FairCLIPlus(torch.nn.Module):
                                                      correlations_with_group[:, None])
             distance_loss += attribute_loss * attr_weight
 
-        total_loss = total_loss_clip + distance_loss
+        total_loss = total_loss_clip + self.fairness_weight * distance_loss
         return total_loss
+
+
+def train_model(model: FairCLIPlus, optimizer, batch_dataset_train: DataLoader,
+                batch_dataset_val: DataLoader, attribute_group_dl: dict[str, dict[int, DataLoader]],
+                result_dir: str, output_model_name:str = "clip.pth", epochs: int = 10,
+                verbose: bool = True, start_epoch: int = 0):
+    """
+    TODO
+    """
+    # Default values
+    best_auc = 0
+    best_acc = 0
+    best_ep = -1
+    best_auc_groups = None
+    best_dpd_groups = None
+    best_eod_groups = None
+    best_es_acc = 0
+    best_es_auc = 0
+    best_between_group_disparity = 0
+    for epoch in range(start_epoch, epochs):
+        model.train()
+        # sanity check
+        if not model.model.training:
+            raise Exception("Internal CLIP model still in evaluation mode")
+
+        # train loop
+        for batch in batch_dataset_train:
+            optimizer.zero_grad()
+
+            images, texts, _ = batch
+
+            logits_per_image, logits_per_text = model(images, texts)
+
+            logits_per_attr = dict()
+
+            for attribute_name, group_dls in attribute_group_dl.items():
+                logits_per_group = dict()
+                for group_idx, group_dl in group_dls.items():
+                    group_images, group_texts, _ = next(group_dl)
+
+                    # We don't want this model to see heaps more data -
+                    # this would make it an unfair comparison
+                    with torch.no_grad():
+                        group_image_logits, group_text_logits = model(group_images, group_texts)
+
+                    logits_per_group[group_idx] = (group_image_logits, group_text_logits)
+
+                logits_per_attr[attribute_name] = logits_per_group
+
+            loss = model.fairCLIPlus_loss(logits_per_image, logits_per_text, logits_per_attr)
+
+            loss.backward()
+            optimizer.step()
+
+        # Eval loop
+        model.eval()
+        # Sanity check:
+        if model.model.training:
+            raise Exception("Internal CLIP model still in training mode")
+
+        # Keep track of all evaluation methods
+        all_probs = []
+        all_labels = []
+        all_attrs = []
+        eval_avg_loss = 0
+        for batch in batch_dataset_val:
+            images, texts, label_and_attributes = batch
+
+            labels = label_and_attributes[:, 0].to(model.device)
+            attributes = label_and_attributes[:, 1:].to(model.device)
+
+            # Zero-shot prediction
+            class_text_feats = []
+            with torch.no_grad():
+                image_features = model.model.encode_image(images)
+                image_features /= image_features.norm(dim=1, keepdim=True)
+
+                for i in range(texts.shape[1]):
+                    text_features = model.model.encode_text(texts[:, i, :])
+                    text_features /= text_features.norm(dim=1, keepdim=True)
+                    class_text_feats.append(text_features[:, None, :])
+                # concatentate class_text_feats along the second dimension
+                class_text_feats = torch.cat(class_text_feats, dim=1)
+
+            # computes a softmax over all similarity diagonals
+            # over all individual features in the batch.
+            # Then computes a softmax over these values
+            vl_prob, _ = compute_vl_prob(
+                image_features, class_text_feats)
+
+            all_probs.append(vl_prob[:, 1].cpu().numpy())
+            all_labels.append(labels.cpu().numpy())
+            all_attrs.append(attributes.cpu().numpy())
+
+            # apply binary cross entropy loss
+            loss = F.binary_cross_entropy(
+                vl_prob[:, 1].float(), labels.float())
+            eval_avg_loss += loss.item()
+
+            # Check if model should be updated:
+
+        all_probs = np.concatenate(all_probs, axis=0)
+        all_labels = np.concatenate(all_labels, axis=0)
+        all_attrs = np.concatenate(all_attrs, axis=0)
+        eval_avg_loss /= len(batch_dataset_val)
+
+        overall_acc, eval_es_acc, overall_auc, eval_es_auc, \
+            eval_aucs_by_attrs, eval_dpds, eval_eods, \
+                between_group_disparity = evalute_comprehensive_perf(
+                                            all_probs, all_labels, all_attrs.T)
+
+        if best_auc <= overall_auc:
+            best_auc = overall_auc
+            best_acc = overall_acc
+            best_ep = epoch
+            best_auc_groups = eval_aucs_by_attrs
+            best_dpd_groups = eval_dpds
+            best_eod_groups = eval_eods
+            best_es_acc = eval_es_acc
+            best_es_auc = eval_es_auc
+            best_between_group_disparity = between_group_disparity
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': eval_avg_loss,
+            }, os.path.join(result_dir, output_model_name))
+
+        # save npz values of each epoch
+        if result_dir is not None:
+            np.savez(os.path.join(result_dir, f'pred_gt_ep{epoch:03d}.npz'),
+                     val_pred=all_probs, val_gt=all_labels, val_attr=all_attrs)
+
+        # log all to output
+        if verbose:
+            logger.log(f'---- best AUC {best_auc:.4f} at epoch {best_ep}')
+            logger.log(
+                f'---- best AUC by groups and attributes at epoch {best_ep}')
+            logger.log(best_auc_groups)
+
+            logger.logkv('epoch', epoch)
+            logger.logkv('trn_loss', round(avg_loss, 4))
+
+            logger.logkv('eval_loss', round(eval_avg_loss, 4))
+            logger.logkv('eval_acc', round(overall_acc, 4))
+            logger.logkv('eval_auc', round(overall_auc, 4))
+
+            for ii, _ in enumerate(eval_es_acc):
+                logger.logkv(f'eval_es_acc_attr{ii}', round(eval_es_acc[ii], 4))
+            for ii, _ in enumerate(eval_es_auc):
+                logger.logkv(f'eval_es_auc_attr{ii}', round(eval_es_auc[ii], 4))
+            for ii, _ in enumerate(eval_aucs_by_attrs):
+                for iii, _ in enumerate(eval_aucs_by_attrs[ii]):
+                    logger.logkv(f'eval_auc_attr{ii}_group{iii}', round(
+                        eval_aucs_by_attrs[ii][iii], 4))
+
+            for ii, _ in enumerate(between_group_disparity):
+                logger.logkv(f'eval_auc_attr{ii}_std_group_disparity', round(
+                    between_group_disparity[ii][0], 4))
+                logger.logkv(f'eval_auc_attr{ii}_max_group_disparity', round(
+                    between_group_disparity[ii][1], 4))
+
+            for ii, _ in enumerate(eval_dpds):
+                logger.logkv(f'eval_dpd_attr{ii}', round(eval_dpds[ii], 4))
+            for ii, _ in enumerate(eval_eods):
+                logger.logkv(f'eval_eod_attr{ii}', round(eval_eods[ii], 4))
+
+            logger.dumpkvs()
+
+    # Done with all epochs
+    return best_auc, best_acc, best_ep, best_auc_groups, best_dpd_groups, \
+            best_eod_groups, best_es_acc, best_es_auc, best_between_group_disparity
---
+++ FairCLIPlus/finetune_fairclip_plus_harvardvlmed.py
@@ -0,0 +1,209 @@
+from src import logger
+from src.modules import *
+import os
+import numpy as np
+import random
+import argparse
+import json
+import pandas as pd
+from geomloss import SamplesLoss
+from FairCLIPlus import FairCLIPlus, train_model
+
+import clip
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch import optim
+import torch.nn.functional as F
+
+import sys
+sys.path.append('.')
+
+
+parser = argparse.ArgumentParser(description='FairCLIP Training/Fine-Tuning')
+
+parser.add_argument('--model_arch', default='ViT-B/16',
+                    type=str, help='options: ViT-B/16 | ViT-L/14')
+parser.add_argument('--pretrained_weights', default='', type=str)
+parser.add_argument(
+  "--attributeslist",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs="*",  # 0 or more values expected => creates a list
+  type=str,
+  default=["race", "gender", "ethnicity", "language"],  # default if nothing is provided
+)
+parser.add_argument(
+  "--weightslist",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs="*",  # 0 or more values expected => creates a list
+  type=float,
+  default=[0.5, 0.5, 0.0, 0.0],  # default if nothing is provided
+)
+parser.add_argument(
+  "--groups_per_attr",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs="*",  # 0 or more values expected => creates a list
+  type=int,
+  default=[3, 2, 2, 3],  # default if nothing is provided
+)
+
+parser.add_argument('--seed', default=-1, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--num_epochs', default=10, type=int)
+parser.add_argument('--lr', '--learning-rate', default=0.05, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=6e-5, type=float,
+                    metavar='W', help='weight decay (default: 6e-5)',
+                    dest='weight_decay')
+parser.add_argument('--batchsize_fairloss', default=64, type=int)
+parser.add_argument('--lambda_fairloss', default=1e-4, type=float)
+parser.add_argument('--sinkhorn_blur', default=1e-4, type=float)
+
+parser.add_argument('--result_dir', default='./results', type=str)
+parser.add_argument('--dataset_dir', default='./data', type=str)
+parser.add_argument('--batch_size', default=32, type=int)
+parser.add_argument('--workers', default=4, type=int)
+parser.add_argument('--summarized_note_file', default='', type=str)
+parser.add_argument('--text_source', default='note',
+                    type=str, help='options: note | label')
+parser.add_argument('--perf_file', default='', type=str)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    if args.seed < 0:
+        args.seed = int(np.random.randint(10000, size=1)[0])
+    set_random_seed(args.seed)
+
+    logger.log(f'===> random seed: {args.seed}')
+
+    result_dir = args.result_dir + f"{args.seed}"
+
+    logger.configure(dir=result_dir, log_suffix='train')
+
+    with open(os.path.join(result_dir, f'args_train.txt'), 'w') as f:
+        json.dump(args.__dict__, f, indent=2)
+
+    # creates final log file
+    best_global_perf_file = os.path.join(
+        os.path.dirname(result_dir), f'best_{args.perf_file}')
+    acc_head_str = ''
+    auc_head_str = ''
+    dpd_head_str = ''
+    eod_head_str = ''
+    esacc_head_str = ''
+    esauc_head_str = ''
+    group_disparity_head_str = ''
+    if args.perf_file != '':
+        if not os.path.exists(best_global_perf_file):
+            for i in range(len(args.groups_per_attr)):
+                auc_head_str += ', '.join(
+                    [f'auc_attr{i}_group{x}' for x in range(args.groups_per_attr[i])]) + ', '
+            dpd_head_str += ', '.join(
+                [f'dpd_attr{x}' for x in range(len(args.groups_per_attr))]) + ', '
+            eod_head_str += ', '.join(
+                [f'eod_attr{x}' for x in range(len(args.groups_per_attr))]) + ', '
+            esacc_head_str += ', '.join(
+                [f'esacc_attr{x}' for x in range(len(args.groups_per_attr))]) + ', '
+            esauc_head_str += ', '.join(
+                [f'esauc_attr{x}' for x in range(len(args.groups_per_attr))]) + ', '
+
+            group_disparity_head_str += ', '.join(
+                [f'std_group_disparity_attr{x}, max_group_disparity_attr{x}' for x in range(len(args.groups_per_attr))]) + ', '
+
+            with open(best_global_perf_file, 'w') as f:
+                f.write(
+                    f'epoch, acc, {esacc_head_str} auc, {esauc_head_str} {auc_head_str} {dpd_head_str} {eod_head_str} {group_disparity_head_str} path\n')
+
+    # Initializing losses
+    loss_img = nn.CrossEntropyLoss()
+    loss_txt = nn.CrossEntropyLoss()
+
+    loss_for_FairCLIP = SamplesLoss(
+        loss="sinkhorn", p=2, blur=args.sinkhorn_blur)
+
+    # Initializing model and optimizer
+    attributes_weights = dict()
+    for i, attr in enumerate(args.attributeslist):
+        attributes_weights[attr] = args.weightslist[i]
+
+    fair_clip_plus = FairCLIPlus(attributes_weights, args.model_arch, device, loss_img, loss_txt,
+                                loss_for_FairCLIP, args.lambda_fairloss, args.pretrained_weights)
+    optimizer = optim.Adam(fair_clip_plus.parameters(), lr=args.lr, betas=(0.1, 0.1),
+                           eps=1e-6, weight_decay=args.weight_decay)
+    start_epoch = 0
+    if args.pretrained_weights != "":
+        checkpoint = torch.load(args.pretrained_weights)
+
+        start_epoch = checkpoint['epoch'] + 1
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    # Loading data
+    train_dataset = fair_vl_med_dataset(args.dataset_dir, fair_clip_plus.preprocess, subset='Training',
+                                        text_source=args.text_source, summarized_note_file=args.summarized_note_file)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+                                  num_workers=args.workers, pin_memory=True, drop_last=False)
+
+    val_dataset = fair_vl_med_dataset(
+        args.dataset_dir, fair_clip_plus.preprocess, subset='Validation')
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+                                num_workers=args.workers, pin_memory=True, drop_last=False)
+
+    test_dataset = fair_vl_med_dataset(
+        args.dataset_dir, fair_clip_plus.preprocess, subset='Test')
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+                                 num_workers=args.workers, pin_memory=True, drop_last=False)
+
+    all_attribute_dataloaders = dict()
+    for attr_index, attr in enumerate(args.groups_per_attr):
+        # get different dataloaders for each group inside an attribute (for example: male, female; or: English, Spanish)
+        group_dataloaders = dict()
+        for group_idx in range(args.group):
+            tmp_dataset = fair_vl_group_dataset(args.dataset_dir, fair_clip_plus.preprocess,
+                                                text_source='note', summarized_note_file=args.summarized_note_file,
+                                                attribute=attr, thegroup=group_idx)
+            tmp_dataloader = DataLoader(tmp_dataset, batch_size=args.batchsize_fairloss, shuffle=True,
+                                        num_workers=args.workers, pin_memory=True, drop_last=False)
+            group_dataloaders[group_idx] = endless_loader(tmp_dataloader)
+        all_attribute_dataloaders[attr] = group_dataloaders
+
+    best_auc, best_acc, best_ep, best_auc_groups, best_dpd_groups, \
+            best_eod_groups, best_es_acc, best_es_auc, \
+            best_between_group_disparity = train_model(fair_clip_plus, optimizer,
+                                                       train_dataloader, val_dataloader,
+                                                       all_attribute_dataloaders, result_dir,
+                                                       start_epoch=start_epoch)
+
+    # Log to corresponding file
+    if args.perf_file != '':
+        if os.path.exists(best_global_perf_file):
+            with open(best_global_perf_file, 'a') as f:
+
+                esacc_head_str = ', '.join(
+                    [f'{x:.4f}' for x in best_es_acc]) + ', '
+                esauc_head_str = ', '.join(
+                    [f'{x:.4f}' for x in best_es_auc]) + ', '
+
+                auc_head_str = ''
+                for i in range(len(best_auc_groups)):
+                    auc_head_str += ', '.join(
+                        [f'{x:.4f}' for x in best_auc_groups[i]]) + ', '
+
+                group_disparity_str = ''
+                for i in range(len(best_between_group_disparity)):
+                    group_disparity_str += ', '.join(
+                        [f'{x:.4f}' for x in best_between_group_disparity[i]]) + ', '
+
+                dpd_head_str = ', '.join(
+                    [f'{x:.4f}' for x in best_dpd_groups]) + ', '
+                eod_head_str = ', '.join(
+                    [f'{x:.4f}' for x in best_eod_groups]) + ', '
+
+                path_str = f'{result_dir}_seed{args.seed}_auc{best_auc:.4f}'
+                f.write(f'{best_ep}, {best_acc:.4f}, {esacc_head_str} {best_auc:.4f}, {esauc_head_str} {auc_head_str} {dpd_head_str} {eod_head_str} {group_disparity_str} {path_str}\n')
+
+    os.rename(result_dir,
+              f'{result_dir}_seed{args.seed}_auc{best_auc:.4f}')
